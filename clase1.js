4b95f6770550d129b2f1f176ca67fdb562dc5801
AntonellaAnnecchini <136344437+AntonellaAnnecchini@users.noreply.github.com>
2023-08-11 09:32:14 -0300

    archivos iniciales javascript

---
+++ clase1.js
@@ -0,0 +1,24 @@
+/*alert('esto es una alerta')
+alert('La cual muestra mensaje, en una ventana emergente')
+
+console. log(120+50)*/
+
+let nombreAlumno = 'Antonella'
+nombreAlumno = nombreAlumno + ' Annecchini'
+alert('Te damos la bienvenida Antonella Annecchini')
+alert('Mi edad es igual a ' + (10+4))
+
+console.log('la suma es igual a', 10+4)
+console.log('la resta es igual a', 10-4)
+console.log('la multiplicación es igual a', 10*4)
+console.log('la división es igual a', 10/4)
+
+nombreAlumno = prompt('Ingresa el nombre completo ')
+apellidoAlumno = prompt('Ingrese el apellido completo')
+alert('Bienvenid@ ' + nombreAlumno + apellidoAlumno)
+
+let Tres = 3
+let Diez = 10
+let resultadoMultiplicación = Tres * Diez
+
+ console.log(resultadoMultiplicación)
